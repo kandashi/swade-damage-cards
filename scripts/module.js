c8360cac54ccb6cbca0484136c7b08b65ae5c740
Kristian Serrano <kristianserrano@gmail.com>
2022-09-11 00:15:38 -0400

    Adds check for selected targets.

--- scripts/module.js
+++ scripts/module.js
@@ -7,7 +7,7 @@ async function applyDamage({ tokenActorUUID, woundsInflicted, statusToApply }) {
         let actor;
         const documentObject = await fromUuid(tokenActorUUID);
         if (documentObject.constructor.name === 'TokenDocument') {
-            actor = documentObject.actor
+            actor = documentObject.actor;
         } else if (documentObject.constructor.name === 'SwadeActor') {
             actor = documentObject;
         }
@@ -26,13 +26,13 @@ async function applyDamage({ tokenActorUUID, woundsInflicted, statusToApply }) {
                             } else if (!actor.isWildcard && game.user.isGM && game.user.bennies > 0) {
                                 game.user.spendBenny();
                             }
-                            await attemptSoak(actor, woundsInflicted, statusToApply, woundsText)
+                            await attemptSoak(actor, woundsInflicted, statusToApply, woundsText);
                         }
                     },
                     soakFree: {
                         label: game.i18n.format("SWDC.soakFree"),
                         callback: async () => {
-                            await attemptSoak(actor, woundsInflicted, statusToApply, woundsText)
+                            await attemptSoak(actor, woundsInflicted, statusToApply, woundsText);
                         }
                     },
                     take: {
@@ -92,7 +92,7 @@ async function attemptSoak(actor, woundsInflicted, statusToApply, woundsText) {
                     label: game.i18n.format("SWDC.rerollSoakFree"),
                     callback: async () => {
                         await attemptSoak(actor, woundsInflicted, statusToApply, woundsText);
-                  }
+                    }
                 },
                 accept: {
                     label: game.i18n.format("SWDC.takeWounds", { wounds: woundsRemainingText }),
@@ -139,52 +139,56 @@ async function applyIncapacitated(actor) {
 
 class DamageCard {
     static render() {
-        new Dialog({
-            title: game.i18n.format("SWDC.title"),
-            content: `
-                <label for="damage">${game.i18n.format("SWADE.Dmg")}</label>
-                <input type="number" id="damage" autofocus>
-                <label for="ap">${game.i18n.format("SWADE.Ap")}</label>
-                <input type="number" id="ap">
-            `,
-            buttons: {
-                calculate: {
-                    label: game.i18n.format("SWDC.calculate"),
-                    callback: async (html) => {
-                        const damage = Number(html.find("#damage")[0].value);
-                        const ap = Number(html.find("#ap")[0].value);
-                        const targets = canvas.tokens.objects.children.filter((t) => t.targeted.size > 0 && !!Array.from(t.targeted).find((u) => u.id === game.userId));
-                        for (const target of targets) {
-                            let { armor, value } = target.actor.data.data.stats.toughness;
-                            if (target.actor.data.type === "vehicle") {
-                                armor = Number(target.actor.data.data.toughness.armor);
-                                value = Number(target.actor.data.data.toughness.total);
-                            }
-                            const apNeg = Math.min(ap, armor);
-                            const newT = value - apNeg;
-                            const excess = damage - newT;
-                            let wounds = Math.floor(excess / 4);
-                            let status = 'none';
-                            if (excess >= 0 && excess < 4) {
-                                status = "shaken";
-                                if (target.actor.data.data.status.isShaken && wounds === 0) {
-                                    wounds = 1;
+        const targets = canvas.tokens.objects.children.filter((t) => t.targeted.size > 0 && !!Array.from(t.targeted).find((u) => u.id === game.userId));
+        if (targets.length) {
+            new Dialog({
+                title: game.i18n.format("SWDC.title"),
+                content: `
+                    <label for="damage">${game.i18n.format("SWADE.Dmg")}</label>
+                    <input type="number" id="damage" autofocus>
+                    <label for="ap">${game.i18n.format("SWADE.Ap")}</label>
+                    <input type="number" id="ap">
+                `,
+                buttons: {
+                    calculate: {
+                        label: game.i18n.format("SWDC.calculate"),
+                        callback: async (html) => {
+                            const damage = Number(html.find("#damage")[0].value);
+                            const ap = Number(html.find("#ap")[0].value);
+                            for (const target of targets) {
+                                let { armor, value } = target.actor.data.data.stats.toughness;
+                                if (target.actor.data.type === "vehicle") {
+                                    armor = Number(target.actor.data.data.toughness.armor);
+                                    value = Number(target.actor.data.data.toughness.total);
+                                }
+                                const apNeg = Math.min(ap, armor);
+                                const newT = value - apNeg;
+                                const excess = damage - newT;
+                                let wounds = Math.floor(excess / 4);
+                                let status = 'none';
+                                if (excess >= 0 && excess < 4) {
+                                    status = "shaken";
+                                    if (target.actor.data.data.status.isShaken && wounds === 0) {
+                                        wounds = 1;
+                                        status = "wounded";
+                                    }
+                                } else if (excess >= 4) {
                                     status = "wounded";
                                 }
-                            } else if (excess >= 4) {
-                                status = "wounded";
+                                game.socket.emit('module.swade-damage-cards', {
+                                    tokenActorUUID: target.actor.uuid,
+                                    woundsInflicted: wounds,
+                                    statusToApply: status
+                                });
                             }
-                            game.socket.emit('module.swade-damage-cards', {
-                                tokenActorUUID: target.actor.uuid,
-                                woundsInflicted: wounds,
-                                statusToApply: status
-                            });
                         }
                     }
-                }
-            },
-            default: "calculate"
-        }, { classes: ["swade-app", "swade-damage-cards"] }).render(true);
+                },
+                default: "calculate"
+            }, { classes: ["swade-app", "swade-damage-cards"] }).render(true);
+        } else {
+            ui.notifications.warn("Please select one or more Targets.");
+        }
     }
 }
 
